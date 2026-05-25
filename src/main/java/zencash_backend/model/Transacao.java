e30fbdbddaa9accf264608d85f7a27dab5da6b7d
Sâmara Jeise Dias <samarajeise@hotmail.com>
2026-05-25 17:27:32 -0300

    Criando entidade Transacao com JPA

---
+++ src/main/java/zencash_backend/model/Transacao.java
@@ -0,0 +1,83 @@
+package zencash_backend.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "TRANSACAO")
+public class Transacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "transacao_seq")
+    @SequenceGenerator(
+            name = "transacao_seq",
+            sequenceName = "SQ_TRANSACAO",
+            allocationSize = 1
+    )
+    @Column(name = "transacao_id")
+    private Long id;
+
+    @Column(name = "conta_origem_id")
+    private Long contaOrigemId;
+
+    @Column(name = "tipo_transacao_id")
+    private Integer tipoTransacaoId;
+
+    @Column(nullable = false)
+    private Double valor;
+
+    @Column(name = "data_hora")
+    private LocalDateTime dataHora;
+
+    private String descricao;
+
+
+    // GETTERS E SETTERS
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getContaOrigemId() {
+        return contaOrigemId;
+    }
+
+    public void setContaOrigemId(Long contaOrigemId) {
+        this.contaOrigemId = contaOrigemId;
+    }
+
+    public Integer getTipoTransacaoId() {
+        return tipoTransacaoId;
+    }
+
+    public void setTipoTransacaoId(Integer tipoTransacaoId) {
+        this.tipoTransacaoId = tipoTransacaoId;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
