f668e40264489d398903c7152fcd7c6f37ea5e66
Sâmara Jeise Dias <samarajeise@hotmail.com>
2026-05-25 17:12:00 -0300

    Criando estrutura inicial do backend Spring Boot

---
+++ src/main/java/zencash_backend/ZencashBackendApplication.java
@@ -0,0 +1,13 @@
+package zencash_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZencashBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZencashBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zencash_backend/ZencashBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package zencash_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZencashBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
